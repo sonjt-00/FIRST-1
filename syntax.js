2a656f513cbcb46330616a63537be72659cb142d
bagjintae-hagbu <>
2021-09-25 07:31:26 +0000

    Initial commit

---
+++ syntax.js
@@ -0,0 +1,31 @@
+// Math
+console.log(1+1);
+console.log(2*4);
+console.log(2-1);
+console.log(3/2);
+
+console.log('\n');
+
+// Variable
+var a = 1;
+console.log(a);
+a = 2 * 19;
+console.log('Now' + a);
+
+console.log('\n');
+
+// String
+var name = 'k8805';
+var letter = 'Dear ' + name + '\n\nLorem ';
+letter = letter + '\n\nFrom ' + name;
+console.log(letter);
+
+console.log('\n');
+
+// Template Literal
+var tlLetter = `Dear ${name}
+
+Lorem
+
+From ${name};`
+console.log(tlLetter);
