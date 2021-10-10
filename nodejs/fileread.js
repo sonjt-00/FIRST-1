f63ba712a43f7fe1f22e873ca493737fe808c998
박진태[ 학부휴학 / 경영학과 ] 학부휴학 <67635342+sonjt-00@users.noreply.github.com>
2021-10-10 07:43:47 +0000

    15, 16, 17 lecture

---
+++ nodejs/fileread.js
@@ -0,0 +1,3 @@
+// https://www.youtube.com/watch?v=X0wfdOJzq_Y&list=PLuHgQVnccGMA9QQX5wqj6ThK7t2tsGxjm&index=17
+
+5분 03부터
--- syntax.js
+++ syntax.js
@@ -1,8 +1,8 @@
 // Math
-console.log(1+1);
-console.log(2*4);
-console.log(2-1);
-console.log(3/2);
+console.log(1 + 1);
+console.log(2 * 4);
+console.log(2 - 1);
+console.log(3 / 2);
 
 console.log('\n');
 
